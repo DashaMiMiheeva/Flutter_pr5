35b34ba1ea3ca600ce89203af03b6e714c0f1143
Mikheeva Daria <dashutami@mail.ru>
2025-10-17 15:18:31 +0300

    Theme edited

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,27 @@ class ExpensesApp extends StatelessWidget {
     return MaterialApp(
       title: 'Учёт расходов',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primaryColor: const Color(0xFFFFC1CC), // основной цвет (нежно-розовый)
+        scaffoldBackgroundColor: const Color(0xFFFFF0F5), // фон
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFFFC1CC),
+          foregroundColor: Colors.white,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFFFC1CC),
+          foregroundColor: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFFFFC1CC),
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFFFFC1CC),
+          secondary: Color(0xFFFFE4E9),
+        ),
         useMaterial3: false,
       ),
       home: const ExpensesContainer(),
